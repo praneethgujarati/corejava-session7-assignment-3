86ad74d3a6f7541edd9318f0617bf50dd3b13bc5
praneethgujarati <praneeth705@gmail.com>
2016-12-03 15:04:30 +0530

    Add files via upload

---
+++ SubString.java
@@ -0,0 +1,30 @@
+public class SubString
+{
+   public static void main(String args[])
+   {
+       
+       String S2="pra";
+       char[] ch2=S2.toCharArray();
+       
+       System.out.print("Enter the string:");
+       String S1=System.console().readLine();
+ 
+       char[] ch1=S1.toCharArray();
+       int i=0,j=0;
+       for(char c:S1.toCharArray())
+  
+       {
+            i++;
+          
+             
+       }
+      
+      System.out.println("sub-string is:" +i);
+       
+       
+        
+       
+
+       
+   }
+}
